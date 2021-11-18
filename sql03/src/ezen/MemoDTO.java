6a5d1ad17efbca01e5470fdb9c8e95535ede1bf3
heojieun <gjekdmlwl@naver.com>
2021-11-18 11:36:03 +0900

    주석 수정

--- sql03/src/ezen/MemoDTO.java
+++ sql03/src/ezen/MemoDTO.java
@@ -13,26 +13,29 @@ public class MemoDTO extends DBManager
 	/* MemoVo를 DB에 저장 */
 	public boolean Insert(MemoVO vo)
 	{
-		//insert 실행
+		//sql구문
 		String sql = "";
 		sql += "insert into memo ";
 		sql += "(mtitle, mnote) ";
 		sql += "values ";
 		sql += "('" + _R(vo.getTitle()) + "','" + _R(vo.getNote()) + "');";
+		
+		//insert 실행
 		return RunSQL(sql);
 	}
 	
 	/* MemoVO를 Select */
 	public MemoVO Select(int no)
 	{
-		//select 실행
+		//sql구문
 		String sql = "";
 		sql += "select * ";
 		sql += "from memo ";
 		sql += "where mno = " + no + ";";
 		
+		//select 실행
 		if (OpenQuery(sql) == false) return null; //쿼리 열기
-		if (GetNext() == false) 	 return null;
+		if (GetNext() == false) 	 return null; //다음값 확인
 
 		MemoVO vo = new MemoVO(); //내보낼 객체
 		vo.setNo(Integer.parseInt(GetValue("mno")));
